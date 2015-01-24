4271f3206182e63c32e236655ece32aa3d158226
csulham <chris.sulham@velir.com>
2015-01-24 16:38:40 -0500

    Getting flickr settings from sitecore item

--- Website/sitecore modules/Shell/MediaEmbedModule/GetMedia.aspx.cs
+++ Website/sitecore modules/Shell/MediaEmbedModule/GetMedia.aspx.cs
@@ -6,14 +6,22 @@ using System.Web;
 using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 using FlickrNet;
+
 using Sitecore.Mvc.Extensions;
 
 namespace Website.sitecore_modules.Shell.MediaEmbedModule
 {
+	using Sitecore.Configuration;
+	using Sitecore.Data;
+	using Sitecore.Data.Items;
+	using Sitecore.Diagnostics;
+
 	public partial class GetMedia : System.Web.UI.Page
 	{
-        private FlickrForm tags = new FlickrForm();
+		private FlickrForm tags = new FlickrForm();
+
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			//var flickr = new Flickr(ConfigurationManager.AppSettings["FlickrApiKey"], ConfigurationManager.AppSettings["FlickrSecret"]);
@@ -32,7 +40,7 @@ namespace Website.sitecore_modules.Shell.MediaEmbedModule
 			//	options.Tags = "Trees";
 			//}
 			//tags.Results = flickr.PhotosSearch(options);
-			
+
 			//MediaEmbedRepeater.DataSource = tags.Results;
 			//MediaEmbedRepeater.DataBind();
 
@@ -51,22 +59,42 @@ namespace Website.sitecore_modules.Shell.MediaEmbedModule
 		[WebMethod()]
 		public static PhotoCollection GetMore(string search, int page)
 		{
-			var flickr = new Flickr(ConfigurationManager.AppSettings["FlickrApiKey"], ConfigurationManager.AppSettings["FlickrSecret"]);
+			var flickrSettings = GetFlickrSettings();
+			if (flickrSettings == null)
+			{
+				Log.Error("Flickr API settings not found, reinstall Media Embed Module.", typeof(GetMedia));
+				return new PhotoCollection();
+			}
+
+			var flickr = new Flickr(flickrSettings.Fields["API Key"].Value, flickrSettings.Fields["API Secret"].Value);
+			flickr.InstanceCacheDisabled = true;
+			
 			var options = new PhotoSearchOptions();
+			int perPage = 9;
+			Int32.TryParse(flickrSettings.Fields["Results Per Page"].Value, out perPage);
+			options.PerPage = perPage;
 			options.Tags = search;
 			options.SafeSearch = SafetyLevel.Moderate;
 			options.Page = page;
-			options.PerPage = 9;
-			
+
 			return flickr.PhotosSearch(options);
+		}
+
+		protected static Item GetFlickrSettings()
+		{
+			using (new DatabaseSwitcher(Factory.GetDatabase("master")))
+			{
+				return Sitecore.Context.Database.GetItem("{0743F83B-6D09-472C-97AB-4D4E4F3F16D1}");
+			}
 		}
 	}
 
+	public class FlickrForm
+	{
+		public string TagList { get; set; }
 
-    public class FlickrForm
-    {
-        public string TagList { get; set; }
-        public PhotoCollection Results { get; set; }
-        public int Page { get; set; }
-    }
+		public PhotoCollection Results { get; set; }
+
+		public int Page { get; set; }
+	}
 }
