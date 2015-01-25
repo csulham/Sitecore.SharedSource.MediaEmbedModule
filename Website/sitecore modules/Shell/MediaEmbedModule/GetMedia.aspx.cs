1c79fdb750807e72ade433635f4f28280734bce6
distaula <distaula@gmail.com>
2015-01-24 20:16:50 -0500

    Fixed first load infinite scroll.

--- Website/sitecore modules/Shell/MediaEmbedModule/GetMedia.aspx.cs
+++ Website/sitecore modules/Shell/MediaEmbedModule/GetMedia.aspx.cs
@@ -48,9 +48,9 @@ namespace Website.sitecore_modules.Shell.MediaEmbedModule
 			int perPage = 9;
 			Int32.TryParse(flickrSettings.Fields["Results Per Page"].Value, out perPage);
 			options.PerPage = perPage;
-			if (string.IsNullOrEmpty(search) && flickrSettings.Fields["Default Tags"].HasValue)
+			if (string.IsNullOrEmpty(search) && flickrSettings.Fields["Default Tags"].HasValue && !string.IsNullOrEmpty(flickrSettings.Fields["Default Tags"].Value))
 				search = flickrSettings.Fields["Default Tags"].Value;
-			if (string.IsNullOrEmpty(search) && page != 1)
+			if (string.IsNullOrEmpty(search))
 				return new PhotoCollection();
 			options.Tags = search;
 			options.SafeSearch = SafetyLevel.Moderate;
